6941f186eb397d753aad9fa6151074a83281049a
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-05-09 22:13:10 +0530

    Get pets visits.

--- src/main/java/org/springframework/samples/petclinic/owner/rest/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/rest/VisitController.java
@@ -1,5 +1,9 @@
 package org.springframework.samples.petclinic.owner.rest;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +15,7 @@ import org.springframework.samples.petclinic.owner.PetRepository;
 import org.springframework.samples.petclinic.visit.Visit;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,6 +42,29 @@ public class VisitController {
 		this.owners=owners;
 	}
 	
+	@GetMapping("")
+	public ResponseEntity<Object>getVisits(@PathVariable("petId")int petId,@PathVariable("ownerId")int ownerId) throws NotFoundException{
+		checkOwner(ownerId);
+		checkOwnerPet(ownerId, petId);
+		List<ExistingVisitForm>visits=getVisits(petId);
+		if(visits.size()==0) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(visits,HttpStatus.OK);
+		}
+	}
+	
+	private List<ExistingVisitForm> getVisits(int petId) {
+		Collection<Visit>existingVisits=this.visits.findByPetId(petId);
+		List<ExistingVisitForm>visits=new ArrayList<>();
+		for(Visit existingVisit:existingVisits) {
+			ExistingVisitForm visit=new ExistingVisitForm();
+			visit=visit.getVisit(existingVisit);
+			visits.add(visit);
+		}
+		return visits;
+	}
+
 	@PostMapping("")
 	public ResponseEntity<Object>saveVisit(@PathVariable("petId")int petId,@PathVariable("ownerId")int ownerId,
 			@Valid @RequestBody NewVisitForm visit,BindingResult result) throws NotFoundException{
